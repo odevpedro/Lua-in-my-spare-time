1f1c421b3bcc9c9c5539ff20e23284d123ce66a8
odevpedro <pedrosschmidt2@gmail.com>
2022-12-26 14:35:43 -0300

    omg my first commit in lua

---
+++ something.lua
@@ -0,0 +1,10 @@
+
+-- this is a comment
+--[[ This is a multiline comment
+that is the second line
+and so on
+]]
+
+-- That is a print, and belive me! one day that will become the new World of warcraft :)
+
+print("I am cool")
