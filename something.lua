06652d4e3bcffda16de1e3b67b434bacc7560089
odevpedro <pedrosschmidt2@gmail.com>
2022-12-26 17:11:46 -0300

    more and more

--- something.lua
+++ something.lua
@@ -14,7 +14,7 @@ local hi = nil  -- null value
 
 -- varibles, we not have to provide the type as a declaration:
 
-local name = "John"
+-- local name = "John"
 local x = 10
 
 -- boolean
@@ -30,4 +30,35 @@ local numbers = {1, 2, 3, 4, 5}   -- array of numbers
 local people = {["John"] = 32,}
 
 
+-- Global variables
+GlobalVar = 20
+
+-- multiple
+local one, two, three = "one", 2, false
+print(two)
+
+
+-- String manager
+local name
 print("I am cool")
+
+local name = "Steve"
+
+-- String concatenation
+print("Hello my name is " .. name)
+print("I have a cool name " .. name)
+
+-- type verify
+local num = 10
+local str = tostring(num)
+print((type(num)), type(str))
+
+print("Hello\nworld\tI am \valmost 19\\20")
+
+
+-- string properties
+
+local myString = "Too easy!! come on this is boring"
+print(myString)
+print(string.lower(myString))
+print (string.upper(myString))
