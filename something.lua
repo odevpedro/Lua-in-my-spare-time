022121ba8a6c634cb9896c1640fd23690a3cad21
odevpedro <pedrosschmidt2@gmail.com>
2022-12-26 15:19:56 -0300

    datatypes and string manipulation

--- something.lua
+++ something.lua
@@ -7,4 +7,27 @@ and so on
 
 -- That is a print, and belive me! one day that will become the new World of warcraft :)
 
+
+-- Data Types & String manipulation
+
+local hi = nil  -- null value
+
+-- varibles, we not have to provide the type as a declaration:
+
+local name = "John"
+local x = 10
+
+-- boolean
+local isActive = true
+local isVisible = false
+
+--String
+local message = "Hello, world!"
+local path = "C:\\Users\\Public\\Documents"
+
+-- Tables
+local numbers = {1, 2, 3, 4, 5}   -- array of numbers
+local people = {["John"] = 32,}
+
+
 print("I am cool")
