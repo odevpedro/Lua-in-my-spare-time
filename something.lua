61d9c056d6049cbce29088be73f41eeac65c6141
odevpedro <pedrosschmidt2@gmail.com>
2023-01-05 17:42:38 -0300

    tables

--- something.lua
+++ something.lua
@@ -1,14 +1,28 @@
+-- Create an empty table
+local t = {}
 
--- Just get the name and return
-print("Enter your name:")
-local name = io.read()
-print("Hello, " .. name .. "!")
+-- Create a table with some initial values
+local t = {1, 2, 3, 4, 5}
+
+-- Create a table with string keys and numeric values
+local t = {a = 1, b = 2, c = 3}
+
+-- Create a table with nested tables
+local t = {a = {1, 2, 3}, b = {4, 5, 6}}
 
 
--- now in the same line of the output 
+-- Access an element of a table
+local t = {1, 2, 3, 4, 5}
+print(t[1]) -- prints 1
 
+-- Modify an element of a table
+t[1] = 10
+print(t[1]) -- prints 10
 
-io.write("Entender your name: ")
-local ans = io.read()
+-- Access a element of a table with a string key
+local t = {a = 1, b = 2, c = 3}
+print(t.a) -- prints 1
 
-print("Name:", ans)
+-- Modify a element of a table with a string key
+t.a = 10
+print(t.a) -- prints 10
